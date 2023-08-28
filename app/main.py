7ca15545247b47cb7417f709cb1c570047ffc461
Peter Paprotny <pappet@web.de>
2023-08-28 22:29:30 +0200

    Reworked the user experience. Now you can select keys from the log and only show them.

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 # Import necessary functions and modules
 from cli import parse_arguments
-from utils import read_log_file, print_log_lines
+from utils import *
 from parsers import parsers_util
 
+
 def main():
-    try:    
+    try:
         # Capture CLI arguments
         args = parse_arguments()
-        
+
         # Check if you just want to print the file lines or parse them
         if args.print:  # Assuming you have a --print option in your CLI
             print_log_lines(args.file_path)
@@ -23,19 +24,34 @@ def main():
             if parsed_line:  # Filter out possible None results
                 parsed_data.append(parsed_line)
 
-        # Further process the parsed data as needed
-        # For instance, you might insert them into a database, save in a file, etc.
+        # If there's any parsed data, process further
+        if parsed_data:
+            keys = list(parsed_data[0].keys())
 
-        # For now, just print the parsed data
-        for entry in parsed_data:
-            print(entry)
+            while True:
+                display_available_keys(keys)
+                selected_keys = get_user_choice(keys)
+
+                if selected_keys == 'q':  # User decided to quit
+                    print("Exiting the program. Goodbye!")
+                    break
+
+                # If the user provides an invalid choice, continue with the loop to re-prompt
+                if not selected_keys:
+                    continue
+
+                print_selected_data(parsed_data, selected_keys)
+        else:
+            print("No data was parsed from the log file.")
 
     except FileNotFoundError as e:
         print(f"Fehler: {e}")
-    except ValueError as e:  # Dies könnte z.B. durch das `get_parser_for_format` ausgelöst werden.
+    # Dies könnte z.B. durch das `get_parser_for_format` ausgelöst werden.
+    except ValueError as e:
         print(f"Ungültiger Wert: {e}")
     except Exception as e:   # Dies fängt allgemeine Fehler ab
         print(f"Ein unerwarteter Fehler ist aufgetreten: {e}")
+
 
 if __name__ == "__main__":
     main()
--- app/utils.py
+++ app/utils.py
@@ -1,10 +1,10 @@
 def read_log_file(file_path):
     """
     Read a log file and yield its lines.
-    
+
     Args:
         file_path (str): Path to the log file to be read.
-    
+
     Yields:
         str: A line from the log file.
     """
@@ -16,9 +16,68 @@ def read_log_file(file_path):
 def print_log_lines(file_path):
     """
     Print each line of the log file.
-    
+
     Args:
         file_path (str): Path to the log file to be printed.
     """
     for line in read_log_file(file_path):
         print(line, end="")  # Prevents double line breaks
+
+
+def display_available_keys(keys):
+    """
+    Display the available keys in the parsed data.
+
+    Args:
+        keys (list): List of keys available in the parsed data.
+    """
+    print("\nAvailable keys in the parsed data:")
+    for idx, key in enumerate(keys, 1):
+        print(f"{idx}. {key}")
+
+
+def get_user_choice(keys):
+    """
+    Prompt the user to select keys by entering their corresponding numbers.
+
+    Args:
+        keys (list): List of keys available in the parsed data.
+
+    Returns:
+        list or None: List of selected keys or None if the user chooses to quit.
+    """
+    choice = input(
+        "\nEnter numbers corresponding to keys from the list above separated by commas (e.g., '1,3') to view their data, or 'q' to quit: ")
+
+    # Remove trailing comma, if present
+    if choice.endswith(","):
+        choice = choice[:-1]
+
+    if choice == 'q':
+        return 'q'
+
+    selected_keys = []
+    for ch in choice.split(","):
+        ch = ch.strip()
+        if ch.isdigit() and 1 <= int(ch) <= len(keys):
+            selected_keys.append(keys[int(ch) - 1])
+        else:
+            print("Invalid choice. Please select valid numbers or 'q' to quit.")
+            return []  # Returning an empty list to indicate invalid choice
+
+    return selected_keys
+
+
+def print_selected_data(parsed_data, selected_keys):
+    """
+    Print the values of the selected keys from the parsed data.
+
+    Args:
+        parsed_data (list): List of dictionaries containing the parsed log entries.
+        selected_keys (list): List of selected keys to print.
+    """
+    for entry in parsed_data:
+        for key in selected_keys:
+            # Separator between values
+            print(entry[key], end="  |  ")
+        print()  # Separator between entries
